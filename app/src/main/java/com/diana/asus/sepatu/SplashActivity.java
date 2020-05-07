d59ba99086b1be38d4310762b5ebbe61a3b538e1
mauluddiana <mauluddiana@email.com>
2020-05-07 11:56:22 +0700

    splash, layout login register

---
+++ app/src/main/java/com/diana/asus/sepatu/SplashActivity.java
@@ -0,0 +1,60 @@
+package com.diana.asus.sepatu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import net.khirr.android.privacypolicy.PrivacyPolicyDialog;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                instantiatePrivacyPolicy();
+            }
+        }, 1000);
+    }
+    public void instantiatePrivacyPolicy(){
+        PrivacyPolicyDialog dialog = new PrivacyPolicyDialog(this,
+                "https://localhost/terms",
+                "https://localhost/privacy");
+
+        dialog.addPoliceLine("This application uses a unique user identifier for advertising purposes, it is shared with third-party companies.");
+        dialog.addPoliceLine("This application sends error reports, installation and send it to a server of the Fabric.io company to analyze and process it.");
+        dialog.addPoliceLine("This application requires internet access and must collect the following information: Installed applications and history of installed applications, ip address, unique installation id, token to send notifications, version of the application, time zone and information about the language of the device.");
+        dialog.addPoliceLine("All details about the use of data are available in our Privacy Policies, as well as all Terms of Service links below.");
+
+        final Intent intent = new Intent(this, MainActivity.class);
+
+        dialog.setOnClickListener(new PrivacyPolicyDialog.OnClickListener() {
+            @Override
+            public void onAccept(boolean isFirstTime) {
+                Log.e("MainActivity", "Policies accepted");
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.e("MainActivity", "Policies not accepted");
+                finish();
+            }
+        });
+
+        //Edit Bimbing Mauluddiana
+        //  Title
+        dialog.setTitle("Terms of Service");
+
+        dialog.show();
+    }
+}
